ee4a8990a0198fb56dce30c33484359ae1f34536
David Gómez <dagove47@gmail.com>
2023-10-06 05:28:41 -0600

    test-4-form

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-(function () {
 
     const loader = document.getElementById("loader");
     const formNewMessage = document.getElementById('formNewMessage');
-    const formInputs = document.querySelectorAll('#newName, #newEmail, #newSubject, #newMessage')
+    const formInputs = document.querySelectorAll('#newParticipant, #newEmail, #newGitHub');
     const submitBtn = document.getElementById('submitBtn');
 
     /* 
@@ -27,38 +26,55 @@
     /* 
         Contact
     */
-    formNewMessage.addEventListener('submit', function (event) {
-        event.preventDefault();
 
-
-        if (!formNewMessage.checkValidity()) {
-            formNewMessage.classList.add('was-validated');
-        } else {
-            submitBtn.value = 'Sending...';
-            const serviceID = 'service_80kcak3';
-            const templateID = 'code_challenge_2023';
-
-            emailjs.sendForm(serviceID, templateID, this)
-                .then(() => {
-                    submitBtn.value = 'Send Registration!';
-                    clearInputs(formInputs);
-                    formNewMessage.classList.remove('was-validated');
-                    alert('Sent!');
-                }, (err) => {
-                    submitBtn.value = 'Send Registration!';
-                    clearInputs(formInputs);
-                    formNewMessage.classList.remove('was-validated');
-                    alert(JSON.stringify(err));
-                });
-        }
-    });
-
-    function clearInputs(inputs) {
-        inputs.forEach(input => {
-            input.value = '';
+        formNewMessage.addEventListener('submit', function(event) {
+           event.preventDefault();
+        
+           submitBtn.value = 'Sending...';
+        
+           const serviceID = 'default_service';
+           const templateID = 'code_challenge_2023';
+        
+           emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                submitBtn.value = 'Send Email';
+              alert('Sent!');
+            }, (err) => {
+                submitBtn.value = 'Send Email';
+              alert(JSON.stringify(err));
+            });
         });
-    }
+
+    // formNewMessage.addEventListener('submit', function(event) {
+    //     event.preventDefault();
 
 
-})();
+    //     if (!formNewMessage.checkValidity()) {
+    //         formNewMessage.classList.add('was-validated');
+    //     } else {
+    //         submitBtn.value = 'Sending...';
+    //         const serviceID = 'default_service';
+    //         const templateID = 'code_challenge_2023';
+
+    //         emailjs.sendForm(serviceID, templateID, this)
+    //             .then(() => {
+    //                 submitBtn.value = 'Send Registration!';
+    //                 clearInputs(formInputs);
+    //                 formNewMessage.classList.remove('was-validated');
+    //                 alert('Sent!');
+    //             }, (err) => {
+    //                 submitBtn.value = 'Send Registration!';
+    //                 clearInputs(formInputs);
+    //                 formNewMessage.classList.remove('was-validated');
+    //                 alert(JSON.stringify(err));
+    //             });
+    //     }
+    // });
+
+    // function clearInputs(inputs) {
+    //     inputs.forEach(input => {
+    //         input.value = '';
+    //     });
+    // }
+
 
