115859369867f0396588283a136509e854b6413f
David Gómez <dagove47@gmail.com>
2023-10-06 06:55:03 -0600

    form updated

--- js/main.js
+++ js/main.js
@@ -1,80 +1,56 @@
 
-    const loader = document.getElementById("loader");
-    const formNewMessage = document.getElementById('formNewMessage');
-    const formInputs = document.querySelectorAll('#newParticipant, #newEmail, #newGitHub');
-    const submitBtn = document.getElementById('submitBtn');
+const loader = document.getElementById("loader");
 
-    /* 
-        Loader
-    */
-    window.addEventListener("load", () => {
-        loader.style.display = "none";
-    })
+const formNewMessage = document.getElementById('formNewMessage');
+const formInputs = document.querySelectorAll('#newName, #newEmail, #newSubject, #newMessage')
+const submitBtn = document.getElementById('submitBtn');
+
+formNewMessage.addEventListener('submit', function (event) {
+    event.preventDefault();
 
 
-    /* 
-        Modal
-    */
-    const myModal = document.getElementById('myModal')
-    const myInput = document.getElementById('myInput')
+    if (!formNewMessage.checkValidity()) {
+        formNewMessage.classList.add('was-validated');
+    } else {
+        submitBtn.value = 'Sending...';
+        const serviceID = 'default_service';
+        const templateID = 'portfolio_form';
 
-    myModal.addEventListener('shown.bs.modal', () => {
-        myInput.focus()
-    })
-
-    
-    /* 
-        Contact
-    */
-
-        formNewMessage.addEventListener('submit', function(event) {
-           event.preventDefault();
-        
-           submitBtn.value = 'Sending...';
-        
-           const serviceID = 'default_service';
-           const templateID = 'code_challenge_2023';
-        
-           emailjs.sendForm(serviceID, templateID, this)
+        emailjs.sendForm(serviceID, templateID, this)
             .then(() => {
-                submitBtn.value = 'Send Email';
-              alert('Sent!');
+                submitBtn.value = 'Send Message!';
+                clearInputs(formInputs);
+                formNewMessage.classList.remove('was-validated');
+                alert('Sent!');
             }, (err) => {
-                submitBtn.value = 'Send Email';
-              alert(JSON.stringify(err));
+                submitBtn.value = 'Send Message!';
+                clearInputs(formInputs);
+                formNewMessage.classList.remove('was-validated');
+                alert(JSON.stringify(err));
             });
-        });
+    }
+});
 
-    // formNewMessage.addEventListener('submit', function(event) {
-    //     event.preventDefault();
+function clearInputs(inputs) {
+    inputs.forEach(input => {
+        input.value = '';
+    });
+}
 
-
-    //     if (!formNewMessage.checkValidity()) {
-    //         formNewMessage.classList.add('was-validated');
-    //     } else {
-    //         submitBtn.value = 'Sending...';
-    //         const serviceID = 'default_service';
-    //         const templateID = 'code_challenge_2023';
-
-    //         emailjs.sendForm(serviceID, templateID, this)
-    //             .then(() => {
-    //                 submitBtn.value = 'Send Registration!';
-    //                 clearInputs(formInputs);
-    //                 formNewMessage.classList.remove('was-validated');
-    //                 alert('Sent!');
-    //             }, (err) => {
-    //                 submitBtn.value = 'Send Registration!';
-    //                 clearInputs(formInputs);
-    //                 formNewMessage.classList.remove('was-validated');
-    //                 alert(JSON.stringify(err));
-    //             });
-    //     }
-    // });
-
-    // function clearInputs(inputs) {
-    //     inputs.forEach(input => {
-    //         input.value = '';
-    //     });
-    // }
+/* 
+    Loader
+*/
+window.addEventListener("load", () => {
+    loader.style.display = "none";
+})
 
 
+/* 
+    Modal
+*/
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+    myInput.focus()
+})
